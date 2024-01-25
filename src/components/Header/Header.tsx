cfe3fcfe614aed307c874aadd5fb5232ca75a0b5
hamidrezajalivar <hamidrezajalivar17830@gmail.com>
2024-01-25 20:52:51 +0330

    getAllByRole,getByRole,queryByText,findByText

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,7 +2,28 @@ const Header = ({title}:string) => {
     return ( 
         <header>
         <h1 title="header">{title}</h1>
-        <h2>header 2</h2>
+        <h2 data-testId="header-2">header 2</h2>
+        <section>
+  <button
+    role="spinbutton"
+    aria-valuenow={5}
+    aria-valuemin={0}
+    aria-valuemax={10}
+    aria-valuetext="medium"
+  >
+    Volume
+  </button>
+  <button
+    role="spinbutton"
+    aria-valuenow={3}
+    aria-valuemin={0}
+    aria-valuemax={10}
+    aria-valuetext="medium"
+  >
+    Pitch
+  </button>
+</section>
+
         </header>
      );
 }
--- src/components/Header/__test__/Header.test.tsx
+++ src/components/Header/__test__/Header.test.tsx
@@ -1,6 +1,9 @@
-import { describe, test,expect } from 'vitest';
+import { describe, test, expect } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import Header from './../Header';
+
+
+/// doc https://testing-library.com/docs/queries/byrole
 describe(" Header", () => {
 
     test("header testing ... #1 ", () => {
@@ -8,10 +11,10 @@ describe(" Header", () => {
         const h1Element = screen.getByText(/react/i);
         expect(h1Element).toBeInTheDocument();
     })
-    
+
     test("header testing ... #2 ", () => {
         render(<Header title="React App" />);
-        const h1Element = screen.getByRole("heading",{name: /react/i});
+        const h1Element = screen.getByRole("heading", { name: /react/i });
         expect(h1Element).toBeInTheDocument();
     })
     test("header testing ... #3 ", () => {
@@ -19,5 +22,48 @@ describe(" Header", () => {
         const h1Element = screen.getByTitle("header");
         expect(h1Element).toBeInTheDocument();
     })
+    test("header testing ... #4 ", () => {
+        render(<Header title="React App" />);
+        const h1Element = screen.getByTestId("header-2");
+        expect(h1Element).toBeInTheDocument();
+    })
 
+    /// find ==> async
+    test("header testing ... #5 ", async () => {
+        render(<Header title="React App" />);
+        const h1Element = await screen.findByText(/react/i);
+        expect(h1Element).toBeInTheDocument();
+    })
+    ///query => for not element
+    test("header testing ... #6 ", () => {
+        render(<Header title="React App" />);
+        const h1Element = screen.queryByText(/vue/i);
+        expect(h1Element).not.toBeInTheDocument();
+
+    });
+
+    test("header testing ... #7 ", () => {
+        render(<Header title="React App" />);
+        const headings = screen.getAllByRole("heading");
+        expect(headings.length).toBe(2);
+
+    });
+    test("header testing ... #8 ", () => {
+        render(<Header title="React App" />);
+        const headings = screen.getAllByRole("heading", { level: 2 });
+        expect(headings.length).toBe(1);
+
+    });
+    test("header testing ... #9 ", () => {
+        render(<Header title="React App" />);
+        const button = screen.getByRole("spinbutton", { value:{now: 5 }});
+        expect(button).toBeInTheDocument();
+
+    });
+    test("header testing ... #10 ", () => {
+        render(<Header title="React App" />);
+        const button = screen.getAllByRole("spinbutton", { value:{min: 0}});
+        expect(button.length).toBe(2);
+
+    });
 })
